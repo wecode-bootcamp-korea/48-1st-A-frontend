ab53dc016001fddc8ae4c3ad822b0b993bcb3878
jini <kjhee0394@gmail.com>
2023-08-12 19:06:30 +0900

    Add : 글쓰기 버튼 기능 구현 - 취소버튼 클릭시 리스트로 이동 - 게시버튼 클릭시 공백이거나 내용이 있을 때 각 경우에 따른 alert창 띄움 - 내용이 있을 경우 게시완료 메시지가 뜨고 리스트로 이동

--- src/pages/Post/PostAdd.js
+++ src/pages/Post/PostAdd.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PostAdd.scss';
+import { useNavigate } from 'react-router-dom';
 
 const Post_add = () => {
-  return <div>hiaa</div>;
+  const [postValue, setPostValue] = useState('');
+
+  const savePost = event => {
+    setPostValue(event.target.value);
+  };
+
+  const navigate = useNavigate();
+  const goToBack = () => {
+    navigate('/post-list');
+  };
+
+  const handleSubmit = () => {
+    if (postValue.length < 1) {
+      alert('내용을 확인해주세요.');
+    } else {
+      alert('게시글이 등록되었습니다.');
+      navigate('/post-list');
+    }
+  };
+
+  return (
+    <article>
+      <div className="container">
+        <div className="profileImg">
+          <img src="\images\user1.png" alt="유저사진" />
+        </div>
+        <div className="content">
+          <p className="userName">Name</p>
+          <textarea
+            value={postValue}
+            onChange={savePost}
+            placeholder="스레드를 시작하세요."
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="bottomBtn">
+          <button className="addCancel" onClick={goToBack} type="button">
+            취소
+          </button>
+          <button
+            className="addPost"
+            value={postValue}
+            onClick={handleSubmit}
+            type="button"
+          >
+            게시
+          </button>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default Post_add;
