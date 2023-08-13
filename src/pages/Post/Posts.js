63adc35791f9d7e239527a5c0a505216037b4eb3
wanwork <wjdqo9705@gmail.com>
2023-08-13 19:52:52 +0900

    Add: Posts 컴포넌트 생성 및 MockData로 동적인 관리 구현 완료

---
+++ src/pages/Post/Posts.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+import './PostList.scss';
+
+const Posts = ({ postData }) => {
+  return (
+    <>
+      {postData.map((postData, index) => (
+        <li key={index} className="postListContainer">
+          <div className="writerDesktop">
+            <div className="userInfoWrap">
+              <img
+                className="profileImg"
+                src={process.env.PUBLIC_URL + postData.userProfile}
+                alt="프로필 이미지"
+              />
+
+              <span className="nameProfile">{postData.nickName}</span>
+            </div>
+            <div className="functionWrap">
+              <span className="dateInfo">00. 00. 00</span>
+              <button className="deleteBtn">삭제</button>
+              <button className="modifyBtn">수정</button>
+            </div>
+          </div>
+
+          <div className="postTextContainer body1">
+            <p>{postData.content}</p>
+          </div>
+          <div className="actionInfo">
+            <div className="actionWrap">
+              <div className="likeInfo">좋아요 {postData.likeCount}</div>
+              <div className="commentInfo">댓글 00</div>
+            </div>
+            <img
+              className="heartIcon"
+              src="/images/heart.svg"
+              alt="하트아이콘"
+            />
+          </div>
+        </li>
+      ))}
+    </>
+  );
+};
+
+export default Posts;
