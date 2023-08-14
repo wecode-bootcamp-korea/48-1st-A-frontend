838758383256014e436cd04490e97f3ab50b281b
jini <kjhee0394@gmail.com>
2023-08-14 17:47:08 +0900

    Add : fetch 추가

--- src/pages/Post/PostAdd/PostAdd.js
+++ src/pages/Post/PostAdd/PostAdd.js
@@ -18,6 +18,16 @@ const Post_add = () => {
     if (content.length < 1) {
       alert('내용을 확인해주세요.');
     } else {
+      fetch('', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          content: content,
+        }),
+      }).then(response => response.json());
+
       alert('게시글이 등록되었습니다.');
       navigate('/post-list');
     }
